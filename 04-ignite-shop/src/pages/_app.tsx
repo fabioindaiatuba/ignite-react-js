87a4bfe140c0c249a8ac844b81d9f613d77f4e7f
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-10-10 15:48:41 -0300

    04-Init ignite shop

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,5 +1,19 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { AppProps } from "next/app";
+import Image from "next/future/image";
 
-export default MyApp;
+import { globalStyles } from "../styles/global";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
+}
